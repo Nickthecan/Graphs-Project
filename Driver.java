5005c2049f4168dac8980a5a7d9bb776c5ce7eb4
Nicholas Amancio <nick.amancio44@gmail.com>
2024-08-31 18:54:33 -0700

    working on the BreadthFirstSearch Algorithm. Still need to find a way to backtrack iteratively. What kind of professor wants us to use a backtracking alg without recursion ???? This is so stupid

--- Driver.java
+++ Driver.java
@@ -1,5 +1,40 @@
+import graphpackage.Graph;
+import java.util.LinkedList;
+
 class Driver {
     public static void main(String[] args) {
-        
+        Graph<Integer> graph = new Graph<>();
+
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addVertex(6);
+        graph.addVertex(7);
+        graph.addVertex(8);
+        graph.addVertex(9);
+
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(1, 5);
+        graph.addEdge(2, 5);
+        graph.addEdge(4, 7);
+        graph.addEdge(5, 6);
+        graph.addEdge(5, 8);
+        graph.addEdge(7, 8);
+        graph.addEdge(6, 3);
+        graph.addEdge(6, 8);
+        graph.addEdge(8, 9);
+        graph.addEdge(3, 2);
+        graph.addEdge(9, 6);
+
+        System.out.println(graph.getVertexCount());
+        System.out.println(graph.getEdgeCount());
+
+        LinkedList<Integer> breadthFirst = graph.breadthFirstSearch(1);
+        for (int number : breadthFirst) {
+            System.out.print(number + " ");
+        }
     }
 }
--- graphpackage/Graph.java
+++ graphpackage/Graph.java
@@ -33,8 +33,8 @@ public class Graph<T> {
 
     public int getEdgeCount() {
         int edges = 0;
-        for (int i = 0; i < getVertexCount(); i++) {
-            edges += map.get(i).size();
+        for (T vertex : map.keySet()) {
+            edges += map.get(vertex).size();
         } 
         return edges;
     }
@@ -57,17 +57,53 @@ public class Graph<T> {
         return false;
     }
 
-    public List<>hasNeighbors(T vertex) {
+    public LinkedList<T> getNeighbors(T vertex) {
         if (map.containsKey(vertex)) {
-            return 
+            return map.get(vertex);
         }
+        return null;
     }
 
-    public void breadthFirstSearch(Map<T, LinkedList<T>> map) {
-        
+    //searches depth by depth
+    //row by row
+    /*  Start at the startingPoint node
+     *  Find the neighbors of the node
+     *  Add to a list of visitedNodes
+     *  Once all the nodes are visited, check child node of the starting node and then do the same process
+     *  Continue until all nodes are visited
+     *  1 2 4 5 6 8 3 9 7
+     */
+    public LinkedList<T> breadthFirstSearch(T startingPoint) {
+        LinkedList<T> breadthFirstList = new LinkedList<>();
+        LinkedList<T> nodesVisited = new LinkedList<>();
+        T backtrackedSpot = startingPoint;
+
+        breadthFirstList.add(startingPoint);
+        nodesVisited.add(startingPoint);
+        while (nodesVisited.size() != getVertexCount()) {
+            LinkedList<T> neighbors = getNeighbors(startingPoint);
+            boolean doesThisNodeHaveAnyNeighborsAtAll = false;
+            for (T neighbor : neighbors) {
+                if (!nodesVisited.contains(neighbor)) {
+                    breadthFirstList.add(neighbor);
+                    nodesVisited.add(neighbor);
+                    doesThisNodeHaveAnyNeighborsAtAll = true;
+                }
+            }
+            if (doesThisNodeHaveAnyNeighborsAtAll) {
+                backtrackedSpot = startingPoint;
+                startingPoint = neighbors.getFirst();
+            }
+            else {
+                startingPoint = backtrackedSpot;
+            }
+        }
+        return breadthFirstList;
     }
     
-    public void depthFirstSearch(Map<T, LinkedList<T>> map) {
+    //searches full depth of children, then moves on to the next
+    //column by column
+    public void depthFirstSearch(T startingPoint) {
 
     }
 }
