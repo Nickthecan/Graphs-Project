84099d8fd9a995296ae6605debf124f42d43b16e
Nicholas Amancio <nick.amancio44@gmail.com>
2024-09-01 00:58:12 -0700

    finished the BFS. Need to work on the DFS now

--- graphpackage/Graph.java
+++ graphpackage/Graph.java
@@ -2,6 +2,8 @@ package graphpackage;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Queue;
+import java.util.Stack;
 
 /*  Adjacency List
  * 
@@ -71,40 +73,46 @@ public class Graph<T> {
      *  Add to a list of visitedNodes
      *  Once all the nodes are visited, check child node of the starting node and then do the same process
      *  Continue until all nodes are visited
-     *  1 2 4 5 6 8 3 9 7
+     *  GOAL: 1 2 4 5 7 6 8 3 9
      */
     public LinkedList<T> breadthFirstSearch(T startingPoint) {
         LinkedList<T> breadthFirstList = new LinkedList<>();
-        LinkedList<T> nodesVisited = new LinkedList<>();
-        T backtrackedSpot = startingPoint;
+        Queue<T> nodesVisited = new LinkedList<>();
 
         breadthFirstList.add(startingPoint);
         nodesVisited.add(startingPoint);
-        while (nodesVisited.size() != getVertexCount()) {
+        while (!nodesVisited.isEmpty()) {
             LinkedList<T> neighbors = getNeighbors(startingPoint);
-            boolean doesThisNodeHaveAnyNeighborsAtAll = false;
             for (T neighbor : neighbors) {
-                if (!nodesVisited.contains(neighbor)) {
+                if (!breadthFirstList.contains(neighbor)) {
                     breadthFirstList.add(neighbor);
                     nodesVisited.add(neighbor);
-                    doesThisNodeHaveAnyNeighborsAtAll = true;
                 }
             }
-            if (doesThisNodeHaveAnyNeighborsAtAll) {
-                backtrackedSpot = startingPoint;
-                startingPoint = neighbors.getFirst();
-            }
-            else {
-                startingPoint = backtrackedSpot;
-            }
+            nodesVisited.remove();
+            startingPoint = nodesVisited.peek();
         }
         return breadthFirstList;
     }
     
     //searches full depth of children, then moves on to the next
     //column by column
+    /*  Start at the startingPoint node
+     *  Find the first child of the node and then keep finding the first child until you cannot anymore
+     *  Then go to the next child of the lowest parent, then work your way back up to the top
+     *  Then you can move on down to the next child
+     *  Goal: 1 2 5 6 3 8 9 4 7
+     */
     public void depthFirstSearch(T startingPoint) {
+        LinkedList<T> breadthFirstList = new LinkedList<>();
+        Stack<T> nodesVisited = new Stack<>();
 
+        breadthFirstList.add(startingPoint);
+        nodesVisited.add(startingPoint);
+        while (!nodesVisited.isEmpty()) {
+            LinkedList<T> neighbors = getNeighbors(startingPoint);
+
+        }
     }
 }
  
